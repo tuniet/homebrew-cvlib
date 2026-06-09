7c43f3df2b688250324cdf6845dacaa8b41a5295
Toni Fdez. <84507881+tuniet@users.noreply.github.com>
2026-06-09 21:04:19 +0200

    Update and rename cvlib.rb to cv.rb

--- Formula/cv.rb
+++ Formula/cv.rb
@@ -1,4 +1,4 @@
-class CvLib < Formula
+class Cv < Formula
   desc "Custom computer vision library"
   homepage "https://github.com/tuniet/cvlib"
   url "https://github.com/tuniet/cvlib/archive/refs/tags/v0.1.0.tar.gz"
